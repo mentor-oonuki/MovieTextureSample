6cbf319817aae2558a375abe70cbe3643a7ee377
mentor-oonuki <mentor.oonuki@gmail.com>
2016-09-12 22:40:23 +0900

    【追加】 スクリプトにコメント追加

--- Assets/Scripts/VideoTexture.cs
+++ Assets/Scripts/VideoTexture.cs
@@ -1,26 +1,35 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+
 public class VideoTexture : MonoBehaviour 
 {
-    public string VideoName;
-    protected MovieTexture SourceMovieTexture;
-    protected Material TargetMaterial;
+    public string VideoName;    // 再生する動画ファイルの名前(拡張子は不要)
+    protected MovieTexture SourceMovieTexture;  // 動画再生元のテクスチャ
+    protected Material TargetMaterial;          // 動画再生先のテクスチャ
 
     void Start()
     {
+        // 再生対象のマテリアル
         TargetMaterial = gameObject.GetComponent<Renderer>().material;
         StartStream(VideoName);
     }
 
+    // 動画ファイルの再生
     protected void StartStream(string filename)
     {
+        // 動画ファイルの読み込み
         SourceMovieTexture = (MovieTexture)Resources.Load(filename, typeof(MovieTexture));
 
+        // 動画再生先のテクスチャにMovieTextureを設定
         TargetMaterial.mainTexture = SourceMovieTexture;
+        // 音声再生用のコンポーネントを追加
         AudioSource audioSource = gameObject.AddComponent<AudioSource>();
         audioSource.clip = SourceMovieTexture.audioClip;
+
+        // 動画再生
         SourceMovieTexture.Play();
+        // 音声再生
         audioSource.Play();
     }
 
--- Assets/Scripts/VideoTextureUI.cs
+++ Assets/Scripts/VideoTextureUI.cs
@@ -3,24 +3,32 @@ using UnityEngine.UI;
 
 public class VideoTextureUI : MonoBehaviour 
 {
-    public string VideoName;
-    protected MovieTexture SourceMovieTexture;
-    protected Texture TargetTexture;
+    public string VideoName;    // 再生する動画ファイルの名前(拡張子は不要)
+    protected MovieTexture SourceMovieTexture;  // 動画再生元のテクスチャ
+    protected Texture TargetTexture;            // 動画再生先のテクスチャ
 
     void Start()
     {
+        // 再生対象のテクスチャ
         TargetTexture = gameObject.GetComponent<Image>().material.mainTexture;
         StartStream(VideoName);
     }
 
+    // 動画ファイルの再生
     protected void StartStream(string filename)
     {
+        // 動画ファイルの読み込み
         SourceMovieTexture = (MovieTexture)Resources.Load(filename, typeof(MovieTexture));
 
+        // 動画再生先のテクスチャにMovieTextureを設定
         TargetTexture = SourceMovieTexture;
+        // 音声再生用のコンポーネントを追加
         AudioSource audioSource = gameObject.AddComponent<AudioSource>();
         audioSource.clip = SourceMovieTexture.audioClip;
+
+        // 動画再生
         SourceMovieTexture.Play();
+        // 音声再生
         audioSource.Play();
     }
 
